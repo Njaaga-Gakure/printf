88ceedbd2a023af072e946b5ed3ace2e79203622
Njaaga-Gakure <njaagagakure@gmail.com>
2022-11-24 23:50:06 +0000

    Fix p_octal

--- p_octal.c
+++ p_octal.c
@@ -1,21 +1,26 @@
 #include "main.h"
+/**
+ * p_octal - prints number in octal
+ * @list: argument list
+ * Return: Return character count
+ */
 
 int p_octal(va_list list)
-{  
+{
 	unsigned int num;
 	int length;
 	char *str;
 	char *rev_s;
-	
+
 	num = va_arg(list, unsigned int);
-	
+
 	if (num == 0)
 		return (_putchar('0'));
 	if (num < 1)
 		return (-1);
 	length = _strlen(num, 8);
 	str = malloc(sizeof(char) * length + 1);
-	
+
 	if (str == NULL)
 		return (-1);
 	for (length = 0; num > 0; length++)
